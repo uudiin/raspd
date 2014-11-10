5bbcf5b9ed2a81c19765ae82884b5b2f83cecbe9
uudiin <77389867@qq.com>
2014-11-10 23:34:27 +0800

    create timer

--- raspd/delay.c
+++ raspd/delay.c
@@ -5,6 +5,15 @@
 
 #include "module.h"
 
+static void delay(unsigned int nanosec)
+{
+    struct timespec t;
+#define NS_PER_S    (1000 * 1000 * 1000)
+    t.tv_sec = (time_t)(nanosec / NS_PER_S);
+    t.tv_nsec = (long)(nanosec % NS_PER_S);
+    nanosleep(&t, NULL);
+}
+
 static int delay_main(int argc, char *argv[])
 {
     static struct option options[] = {
