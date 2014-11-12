0812789239028302fa67970ae250d04a9cb993f6
kcrazy <thekcrazy@gmail.com>
2014-11-12 13:37:40 +0800

    android: do some cleanup

--- client/android/TankController/src/com/kongdashi/tankcontroller/MainActivity.java
+++ client/android/TankController/src/com/kongdashi/tankcontroller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kongdashi.tankcontroller;
 
+
 import com.kongdashi.tankcontroller.util.SystemUiHider;
 import com.tank.nativef.TankNative;
 
@@ -7,8 +8,6 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,23 +20,6 @@ import android.widget.Toast;
  * @see SystemUiHider
  */
 public class MainActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = false;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
 
     /**
      * The flags to pass to {@link SystemUiHider#getInstance}.
