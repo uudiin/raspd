0e3ee35ccbb464d90ab6a02f16cb47058bd92264
uudiin <uudiin@live.cn>
2014-12-13 16:57:56 +0800

    config: fix pin

--- raspd/config.lua
+++ raspd/config.lua
@@ -10,19 +10,23 @@ script = "control.lua"
 -- automatic function
 automatic = "automatic"
 
+-- pin pwm
+-- channel-0  12 18
+-- channel-1  13 19
+
 -- pin of peripherals
 pin_led_warn = 16
-pin_infrared_sensor = XX
-pin_laser = XX
-pin_voice_sensor = XX
+pin_infrared_sensor = 23
+pin_laser = 24
+pin_voice_sensor = 25
 
 -- ultrasonic
 ultrasonic = {
     ["TRIG"] = 20,
     ["ECHO"] = 21,
 
-    -- urgent scope, 10 cm
-    ["threshold"] = 10,
+    -- urgent scope, 15 cm
+    ["threshold"] = 15,
     ["callback"] = "cb_ultrasonic_urgent",
 }
 
@@ -30,8 +34,8 @@ ultrasonic = {
 l298n = {
     ["ENA"] = 18,
     ["ENB"] = 13,
-    ["IN1"] = 17,
-    ["IN2"] = 27,
-    ["IN3"] = 22,
-    ["IN4"] = 23,
+    ["IN1"] = 27,
+    ["IN2"] = 22,
+    ["IN3"] = 5,
+    ["IN4"] = 6,
 }
