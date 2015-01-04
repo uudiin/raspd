1c4798bf868df7ddbb369c7d84d832f61a2f8447
uudiin <uudiin@live.cn>
2015-01-04 17:32:02 +0800

    optimize devtree

--- raspd/config.lua
+++ raspd/config.lua
@@ -47,47 +47,74 @@ tank = {
 devroot = {
     gpio = {
         ultrasonic = {
-            ID = 1,
-            NAME = "ultrasonic",
+            {
+                ID = 1,
+                NAME = "ultrasonic",
 
-            pin_trig = 20,
-            pin_echo = 21,
+                pin_trig = 20,
+                pin_echo = 21,
 
-            -- keeping pin_trig 10 us in HIGH level
-            trig_time = 10
+                -- keeping pin_trig 10 us in HIGH level
+                trig_time = 10
+            }
         },
 
         stepmotor = {
-            ID = 2,
-            NAME = "stepmotor",
+            {
+                ID = 2,
+                NAME = "stepmotor",
 
-            pin1 = 2,
-            pin2 = 3,
-            pin3 = 4,
-            pin4 = 17,
+                pin1 = 2,
+                pin2 = 3,
+                pin3 = 4,
+                pin4 = 17,
 
-            step_angle = 5.625,
-            reduction_ratio = 64,
-            pullin_freq = 500,
-            pullout_freq = 900,
-            flags = 2           -- SMF_PULSE_EIGHT
-        }
+                step_angle = 5.625,
+                reduction_ratio = 64,
+                pullin_freq = 500,
+                pullout_freq = 900,
+                flags = 2           -- SMF_PULSE_EIGHT
+            }
+        },
+
         l298n = {
-            ID = 3,
-            NAME = "l298n",
+            {
+                ID = 3,
+                NAME = "l298n",
 
-            ena = 18,
-            enb = 13,
-            in1 = 27,
-            in2 = 22,
-            in3 = 5,
-            in4 = 6,
+                ena = 18,
+                enb = 13,
+                in1 = 27,
+                in2 = 22,
+                in3 = 5,
+                in4 = 6,
 
-            max_speed = 5,
-            range = 50000,
-            pwm_div = 16,
+                max_speed = 5,
+                range = 50000,
+                pwm_div = 16,
+            }
+        },
+
+        esc = {
+            {
+                ID = 11,
+                NAME = "esc_a",
+            },
+            {
+                ID = 12,
+                NAME = "esc_b",
+            },
+            {
+                ID = 13,
+                NAME = "esc_c",
+            },
+            {
+                ID = 14,
+                NAME = "esc_d",
+            }
         }
     },
 
-    i2c = {}
+    i2c = {},
+    spi = {}
 }
